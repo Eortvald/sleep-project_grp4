95d1f38e08544a5616c3d923b5c877b82102dab7
nullerh <80833969+nullerh@users.noreply.github.com>
2023-05-04 18:08:17 +0200

    4gpu

--- submit_titans_job.py
+++ submit_titans_job.py
@@ -26,7 +26,8 @@ def submit_job(jobname, partition, time, reservation, ncpus, gpus, command, memo
 #SBATCH --time={time}
 #SBATCH -p {partition}
 #SBATCH --cpus-per-task={ncpus}
-#SBATCH --gres=gpu:{gpus}
+#SBATCH  --gpus-per-node:{gpus}
+
 #SBATCH --mem={memory}
 #SBATCH --output={log_path}/{jobname}.out
 #SBATCH --error={log_path}/{jobname}.err
