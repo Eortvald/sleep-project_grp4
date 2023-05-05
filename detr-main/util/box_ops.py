17bb0e8d6bc876dd64272752654345185dffc459
nullerh <80833969+nullerh@users.noreply.github.com>
2023-05-05 18:22:00 +0200

    nproc_per_node=1

--- detr-main/util/box_ops.py
+++ detr-main/util/box_ops.py
@@ -54,16 +54,14 @@ def generalized_box_iou(boxes1, boxes2):
     """
     # boxes1 = prediction_boxes -- boxes2 = target_boxes #
     # degenerate boxes gives inf / nan results, so do an early check
-    if not (boxes1[:, 2] >= boxes1[:, 0]).all():
-        m = (boxes1[:, 2] < boxes1[:, 0])
-        boxes1[m, 0] = boxes1[m, 2]
+    m1 = (boxes1[:, 2] < boxes1[:, 0])
+    boxes1[m1, 0] = boxes1[m1, 2]
 
-    if not (boxes2[:, 2] >= boxes2[:, 0]).all():
-        m = (boxes2[:, 2] < boxes2[:, 0])
-        boxes2[m, 0] = boxes2[m, 2]
+    m2 = (boxes2[:, 2] < boxes2[:, 0])
+    boxes2[m2, 0] = boxes2[m2, 2]
 
-    #assert (boxes1[:, 2] >= boxes1[:, 0]).all()
-    #assert (boxes2[:, 2] >= boxes2[:, 0]).all()
+    assert (boxes1[:, 2] >= boxes1[:, 0]).all()
+    assert (boxes2[:, 2] >= boxes2[:, 0]).all()
 
     iou, union = box_iou(boxes1, boxes2)
 
