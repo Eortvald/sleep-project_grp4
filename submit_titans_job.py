a2dcd5db5835fc49c48b131b6d5237a30473bb78
nullerh <80833969+nullerh@users.noreply.github.com>
2023-05-05 11:57:21 +0200

    nproc_per_node=4

--- submit_titans_job.py
+++ submit_titans_job.py
@@ -11,7 +11,7 @@ JOBS = [
         "ncpus": 4,  # Number of CPU cores
         "gpus": 4,  # Number of GPUs
         "memory": "64G",  # This is total RAM, change this accordingly to use
-        "command": "python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py detr-main/main.py",
+        "command": "python -m torch.distributed.launch --nproc_per_node=4 --use_env detr-main/main.py",
         "log_path": "/scratch/s194277/new_hyper"  # Usually this is your scratch space
 
     },
